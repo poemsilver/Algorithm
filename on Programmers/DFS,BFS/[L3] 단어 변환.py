b4c1adb643225ed0a445e2ff5e90c97fdf6aef71
poemsilver <120539836+poemsilver@users.noreply.github.com>
2022-12-21 22:24:15 +0900

    Create [L3] 단어 변환.py

---
+++ on Programmers/DFS,BFS/[L3] 단어 변환.py
@@ -0,0 +1,32 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43163
+
+from collections import defaultdict, deque
+def solution(begin, target, words):
+    answer = 0
+    # words에 존재하지 않으면 0
+    if target not in words:
+        return 0
+    
+    # key: 단어, value = 쌓인 변환 횟수
+    visit = defaultdict(int)
+    visit[begin] = 0
+    q = deque([[begin,0]])
+    
+    while q:
+        w,cnt = q.popleft()
+        if w == target:
+            answer = cnt
+        
+        #단어 하나만 다르면 q에 넣기
+        for i in range(len(words)):
+            dif = 0
+            for j in range(len(w)):
+                if dif >= 2:
+                    break
+                if w[j] != words[i][j]:
+                    dif += 1
+            if dif == 1 and visit[words[i]] == 0:
+                visit[words[i]] = cnt+1
+                q.append([words[i],cnt+1])
+    
+    return answer
