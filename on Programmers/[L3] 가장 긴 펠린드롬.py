918be80288598381dd513aee8147dd6c1219ce63
poemsilver <120539836+poemsilver@users.noreply.github.com>
2023-01-02 00:03:40 +0900

    Create [L3] 가장 긴 펠린드롬.py

---
+++ on Programmers/[L3] 가장 긴 펠린드롬.py
@@ -0,0 +1,29 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12904#
+
+def solution(s):
+    answer = 0
+    
+    l = len(s)
+    
+    if len(s) == 1:
+        return 1
+    
+    while l != 1:
+        cnt = 1
+        
+        for j in range(len(s)-l+1):
+            cnt = 1
+            for i in range(l//2):
+                if s[j+i] == s[l+j-i-1]:
+                    cnt+=2
+                else:
+                    break
+            if cnt >= l:
+                break
+        
+        if cnt >= l:
+            break
+        else:
+            l -= 1
+
+    return l
