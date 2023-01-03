db01460def42effb37d5f4d84262a308d2d05fa8
poemsilver <120539836+poemsilver@users.noreply.github.com>
2023-01-03 23:55:09 +0900

    Update [L3] 가장 먼 노드.py

--- on Programmers/DFS,BFS/[L3] 가장 먼 노드.py
+++ on Programmers/DFS,BFS/[L3] 가장 먼 노드.py
@@ -1,4 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/49189
+
+#풀이 1
 from collections import defaultdict,deque
 
 def solution(n, edge):
@@ -39,4 +41,39 @@ def solution(n, edge):
             answer+=1
         else:
             break
+    return answer
+
+
+# 풀이2
+
+from collections import defaultdict,deque
+def solution(n, edge):
+    answer = 0
+    node = defaultdict(list)
+    for i,j in edge:
+        node[i].append(j)
+        node[j].append(i)
+        
+    visit = [50001 for _ in range(n+1)]
+    visit[0] = 0
+    visit[1] = 0
+    
+    q = deque([[1,1]])
+    
+    while q:
+        p,cnt = q.popleft()
+        
+        for i in node[p]:
+            if cnt < visit[i]:
+                visit[i] = cnt
+                q.append([i,cnt+1])
+                    
+    visit.sort(reverse = True)
+    M = visit[0]
+    for i in visit:
+        if i == M:
+            answer += 1
+        else:
+            break
+            
     return answer
