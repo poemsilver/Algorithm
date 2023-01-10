8e14ea6a79c563b2ba6fbc91634c360e1d935f54
poemsilver <120539836+poemsilver@users.noreply.github.com>
2023-01-10 22:56:48 +0900

    Update [L3] 야근 지수.py

--- on Programmers/[L3] 야근 지수.py
+++ on Programmers/[L3] 야근 지수.py
@@ -4,15 +4,16 @@ from heapq import heappush,heappop,heapify
 def solution(n, works):
     answer = 0
     
+    if sum(works) <= n:
+        return 0
+    
     w = [-i for i in works]
     heapify(w)
     
     for _ in range(n):
-        m = -(heappop(w))
-        m -= 1
-        if -m > 0:
-            return 0
-        heappush(w,-m)
+        m = heappop(w)
+        m += 1
+        heappush(w,m)
     
     while w:
         answer += heappop(w)**2
