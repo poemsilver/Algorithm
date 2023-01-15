56e250b529e1b5f959404f292cc5e62913290a17
poemsilver <120539836+poemsilver@users.noreply.github.com>
2023-01-15 15:27:39 +0900

    Update [L3] 합승 택시 요금.py

--- on Programmers/[L3] 합승 택시 요금.py
+++ on Programmers/[L3] 합승 택시 요금.py
@@ -2,10 +2,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3
 
 from collections import defaultdict, deque
+from queue import PriorityQueue
 def solution(n, s, a, b, fares):
     answer = 0
+    if n == 3:
+        return fares[0][2]+fares[1][2]+fares[2][2]
     
-    fares.sort(key = lambda x:x[2])
     graph = defaultdict(list)
     
     for i,j,c in fares:
@@ -25,12 +27,11 @@ def solution(n, s, a, b, fares):
             if costs[i] > c+co:
                 costs[i] = c+co
                 q.append([i,c+co])
-    print(costs)
     
     # A지점부터 B지점까지 최단거리, 합승지점 구하기
     atob = [100001*n for _ in range(n+1)]
     atob[0] = 0
-    atob[s] = 0
+    atob[a] = 0
     q = deque()
     q.append([a,0,[a]])
     node = []
@@ -39,13 +40,18 @@ def solution(n, s, a, b, fares):
         
         if now == b:
             node = path
-            break
         
         for i,co in graph[now]:
             if atob[i] > c+co:
                 atob[i] = c+co
-                path.append(i)
-                q.append([i,c+co,path])
-    print(atob)
-    print(node)
+                q.append([i,c+co,path+[i]])
+    
+    pq = PriorityQueue()
+    
+    for i in node:
+        pq.put((costs[i],i))
+    
+    mid = pq.get()
+    answer = atob[b] + mid[0]
+    
     return answer
